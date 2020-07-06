b6f75474060c32f2de69b9f92fc5cb0f4a8c87d3
Laurecia <KezaLaurence@gmail.com>
2020-07-06 13:25:21 +0400

    ajouter le fichier sur add

---
+++ addTwoInt.py
@@ -0,0 +1,12 @@
+def add():
+
+    a=int(input("veuillez entrez la premiere valeur:"))
+    b=int(input("veuillez entrez la deuxieme valeur:"))
+    print(a+b)
+
+def main():
+    add()
+
+if __name__=="__main__":
+   main()
+
